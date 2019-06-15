2c3b2902efaae5471f41ee40cc6cd36d7232dcac
dev-yakuza <dev.yakuza@gmail.com>
2019-06-15 21:37:00 +0900

    change file name

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    'js/app': ['./src/index.jsx'],
+    'js/app': ['./src/App.jsx'],
   },
   output: {
     path: path.resolve(__dirname, 'dist/'),
